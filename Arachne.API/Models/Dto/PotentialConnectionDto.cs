c25ecedfacb7c59ac6ef4a6bdbfabce6ca4afb98
Rens <github@rensrooimans.nl>
2020-05-04 23:08:37 +0200

    Use package instead of own source

--- Arachne.API/Models/Dto/PotentialConnectionDto.cs
+++ Arachne.API/Models/Dto/PotentialConnectionDto.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
-using Arachne.API.Utilities.Option;
 using Arachne.Data.Models;
+using SomeNone;
+using SomeNone.Extensions;
 
 namespace Arachne.API.Models.Dto
 {
