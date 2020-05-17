92b8c22780cac5cfe40e469a28a7b2b86823b0cd
Rens <github@rensrooimans.nl>
2020-05-17 14:20:18 +0200

    remove easyauth

--- Arachne.API/Functions/ConnectionFunctions.cs
+++ Arachne.API/Functions/ConnectionFunctions.cs
@@ -1,26 +1,30 @@
 using System;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Arachne.API.Models.Dto;
-using Arachne.API.Services;
+using Arachne.API.Repositories.Interfaces;
+using Arachne.API.Services.Interfaces;
 using Arachne.API.Utilities.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Arachne.API.Functions
 {
     public class ConnectionFunctions
     {
-        private readonly ConnectionService _connectionService;
+        private readonly IConnectionService _connectionService;
+        private readonly IPotentialConnectionRepository _potentialConnectionRepository;
 
-        public ConnectionFunctions(ConnectionService connectionService)
+        public ConnectionFunctions(IConnectionService connectionService, IPotentialConnectionRepository potentialConnectionRepository, HttpClient httpClient)
         {
             _connectionService = connectionService;
+            _potentialConnectionRepository = potentialConnectionRepository;
         }
         
         [FunctionName("CreatePotentialConnection")]
-        public async Task<IActionResult> GetUserById(
+        public IActionResult CreatePotentialConnection(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/potentialconnection/create")]
             HttpRequest req)
         {
@@ -29,5 +33,16 @@ namespace Arachne.API.Functions
             
             return new OkObjectResult(PotentialConnectionDto.FromDal(newConnection));
         }
+
+        [FunctionName("GetPendingConnections")]
+        public IActionResult GetPendingConnections(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/potentialconnection/pending")]
+            HttpRequest req, ILogger log)
+        {
+ 
+            var potentialConnections = _potentialConnectionRepository.GetAllPotentialConnectionForUser(Guid.Empty);
+            return new OkObjectResult(potentialConnections);
+        }
+        
     }
 }
